0c8e86e3fab6e294adbcbb9a9fb40dbe6b721871
SiddikX <siddik.iitju@gmail.com>
2023-08-01 02:18:38 +0600

    added loading icon

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ function handleUpload() {
     formData.append('size', 'auto');
 
     const apiKey = "kM8mJ15JaA5YX4E8EpHHqcfE";
+
+    // Show the loading icon when the upload button is clicked
+    const loadingIcon = document.createElement('div');
+    loadingIcon.className = 'loading-icon show';
+    document.body.appendChild(loadingIcon);
+
     fetch('https://api.remove.bg/v1.0/removebg', {
         method: 'POST',
         headers: {
@@ -28,23 +34,30 @@ function handleUpload() {
             // Display the background-removed image
             const img = document.createElement('img');
             img.src = url;
-            img.style.display = 'block'; // Display the image as a block element
+            img.style.display = 'block';
             img.style.margin = "0 auto";
-            img.style.border = "1px solid black"
+            img.style.border = '1px solid black';
             img.style.marginBottom = '10px';
             img.style.marginTop = '10px';
             document.body.appendChild(img);
 
             // Display the download button below the image
             const downloadButton = document.createElement('button');
-            downloadButton.className = 'btn btn-outline-success';
+            downloadButton.className = 'btn btn-outline-dark';
             downloadButton.innerText = 'Download';
-            downloadButton.style.margin = "0 auto";
+            downloadButton.style.margin = '0 auto';
+            downloadButton.style.marginBottom = '20px';
             downloadButton.onclick = downloadFile;
             document.body.appendChild(downloadButton);
+
+            // Hide the loading icon once the image processing is completed
+            loadingIcon.classList.remove('show');
         })
         .catch(function (error) {
             console.error('Error:', error);
+
+            // Hide the loading icon in case of an error as well
+            loadingIcon.classList.remove('show');
         });
 
     console.log("clicked");
